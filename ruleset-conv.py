a2bed50b636b4b535828148c7a78e6db485db670
Yorun <547747006@qq.com>
2022-12-11 19:32:21 +0800

    chore: update script

--- ruleset-conv.py
+++ ruleset-conv.py
@@ -1,86 +1,108 @@
-import os
 import base64
+import functools
+import os
+from typing import Callable, Final
+
 import requests
 import yaml
 
-
 """
-文档地址: https://github.com/tindy2013/subconverter/blob/master/README-cn.md#%E8%A7%84%E5%88%99%E8%BD%AC%E6%8D%A2
-接口示例: https://sub.xeton.dev/getruleset?type=4&url=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/LocalAreaNetwork.list
+文档地址: https://github.com/tindy2013/subconverter/blob/master/README-cn.md#%E8%A7%84%E5%88
+%99%E8%BD%AC%E6%8D%A2
+接口示例: https://sub.xeton.dev/getruleset?type=4&url=https://raw.githubusercontent.com/ACL4
+SSR/ACL4SSR/master/Clash/LocalAreaNetwork.list
 转换类型: 3->domain rule, 4->ipcidr rule
 """
-conv_interface = 'https://sub.xeton.dev/getruleset?'
-conv_type = (3, 4)
-target_dir = './rule/'
-target_file = './rule-providers.yaml'
+CONV_INTERFACE: Final[str] = 'https://sub.xeton.dev/getruleset?'
+CONV_TYPE: Final[tuple[int, int]] = (3, 4)
+TARGET_DIR: Final[str] = './rule/'
+TARGET_FILE: Final[str] = './rule-providers.yaml'
 
 
-def url_base64_encode(url):
+def init(func: Callable) -> Callable:
+    """初始化检查,清理已存在的rule、rule-providers"""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        if os.path.exists(TARGET_DIR):
+            for filename in os.listdir(TARGET_DIR):
+                os.remove(os.path.join(TARGET_DIR, filename))
+        else:
+            os.mkdir(TARGET_DIR)
+
+        if os.path.exists(TARGET_FILE):
+            os.remove(TARGET_FILE)
+
+        return func(*args, **kw)
+
+    return wrapper
+
+
+def url_base64_encode(url: str) -> str:
+    """将string格式的url编码为base64格式的url"""
     return base64.b64encode(url.encode('utf-8')).decode('utf-8')
 
 
-def main(generate_rule_providers=True):
-    rule_url_list = []
-    rule_name_list = []
+@init
+def main(generate_rule_providers: bool = True) -> None:
+    ruleset_url_list: list[str] = []
+    ruleset_name_list: list[str] = []
 
-    # 初始化检查，清理已存在的rule、rule-providers
-    if os.path.exists(target_dir):
-        for target_filename in os.listdir(target_dir):
-            os.remove(os.path.join(target_dir, target_filename))
-    else:
-        os.mkdir(target_dir)
+    # 从ruleset-url.txt读取ruleset url
+    with open('./ruleset-url.txt', 'r') as fr:
+        ruleset_url_list.extend(map(lambda x: x.rstrip('\n'), fr.readlines()))
 
-    if os.path.exists(target_file):
-        os.remove(target_file)
+    # 通过转换接口生成ruleset content并保存到TARGET_DIR
+    for ruleset_url in ruleset_url_list:
+        ruleset_name_prefix: str = ruleset_url.split('/')[-1].split('.')[0]
 
-    # 读取rule url
-    with open('./rule-url.txt', 'r') as fr:
-        rule_url_list = list(map(lambda x: x.rstrip('\n'), fr.readlines()))
+        for TYPE in CONV_TYPE:
+            ruleset_name_suffix: str = 'domain' if TYPE == 3 else 'ipcidr'
 
-    # 生成并保存rule set
-    for rule_url in rule_url_list:
-        rule_name_prefix = rule_url.split('/')[-1].split('.')[0]
-
-        for type in conv_type:
             # 拼接base64预处理url
             request_url = '{}type={}&url={}'.format(
-                conv_interface, str(type), url_base64_encode(rule_url)
+                CONV_INTERFACE, str(TYPE), url_base64_encode(ruleset_url)
             )
 
-            # 下载转换后的rule
-            response_content = requests.get(request_url).content
+            # 下载转换后的ruleset content
+            response_content: bytes = requests.get(request_url).content
 
-            # 跳过内容为空的规则集
+            # 跳过内容为空的ruleset
             if response_content != b"payload:\n  - '0.0.0.0/32'":
-                # 拼接rule name
-                rule_name_suffix = '_domain' if type == 3 else '_ipcidr'
-                rule_name = rule_name_prefix + rule_name_suffix
-                rule_name_list.append(rule_name)
+                # 拼接ruleset name
+                ruleset_name: str = "_".join([ruleset_name_prefix, ruleset_name_suffix])
+                ruleset_name_list.append(ruleset_name)
 
-                # 保存rule
-                with open(os.path.join(target_dir, rule_name) + '.yaml', 'wb') as fw:
+                # 保存ruleset content
+                with open(os.path.join(TARGET_DIR, ruleset_name) + '.yaml', 'wb') as fw:
                     fw.write(response_content)
 
+    # 生成rule-providers并保存到TARGET_FILE
     if generate_rule_providers is True:
-        # 生成并保存rule-providers
-        dump = {}
-        for name in rule_name_list:
+        dump: dict[str, dict[str, str | int]] = {}
+        root: dict[str, dict] = {'rule-providers': dump}
+
+        for ruleset_name in ruleset_name_list:
             # rule-providers模板
-            dump[name] = {
+            dump[ruleset_name] = {
                 'type': 'http',
-                'behavior': name.split('_')[-1],
-                'url': 'https://cdn.jsdelivr.net/gh/yorunning/clash_conf@main/rule/%s.yaml'
-                % name,
+                'behavior': ruleset_name.split('_')[-1],
+                'url': ''.join(
+                    [
+                        'https://cdn.jsdelivr.net/gh/yorunning/clash_conf@main/rule/',
+                        ruleset_name,
+                        '.yaml',
+                    ]
+                ),
                 'interval': 86400,
             }
-        root = {'rule-providers': dump}
 
-        # 生成rule-providers文本并保存
-        rule_providers_content = yaml.dump(root, sort_keys=False).replace(
+        # dict转换为yaml格式
+        rule_providers_content: str = yaml.dump(root, sort_keys=False).replace(
             '86400', '86400\n'
         )
-
-        with open(target_file, 'w') as fw:
+        # 保存rule-providers
+        with open(TARGET_FILE, 'w') as fw:
             fw.write(rule_providers_content)
 
 
